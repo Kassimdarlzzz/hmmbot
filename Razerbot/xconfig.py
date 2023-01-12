a4f2a736164a90c07b8b90d8768c12722f5c4b0f
Kassimdarlzzz <113440042+Kassimdarlzzz@users.noreply.github.com>
2023-01-12 16:19:35 +0530

    Update xconfig.py

--- Razerbot/xconfig.py
+++ Razerbot/xconfig.py
@@ -10,30 +10,30 @@ def get_user_list(config, key):
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "API_HASH"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    BOT_USERNAME = "BOT_USERNAME"
-    BOT_NAME = ""
-    BOT_ID = ""
-    OWNER_ID = Your_Id  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "YourUsername"
-    START_IMG = "https://graph.org/file/644fddccf30ac191fc895.jpg"
-    ALIVE_IMG = "https://graph.org/file/36c17c0f22aeea9c99895.jpg"
-    UPDATE_CHANNEL = "<channel-username>" # Your own channel for updates, do not add the @
-    SUPPORT_CHAT = "<support-chat-username>"  # Your own group for support, do not add the @
+    API_ID = 18641113  # integer value, dont use ""
+    API_HASH = "bc5fea81e7bf9f3c0784a0a7d35f9c71"
+    TOKEN = "5864142861:AAHRGgDQnqDoWaHhP0k_ginuFMpeuDBmrPk"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    BOT_USERNAME = "WizkhalifaBot"
+    BOT_NAME = "Wizkhalifa"
+    BOT_ID = "5864142861"
+    OWNER_ID = 5323266323  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "kassim_darlzzz"
+    START_IMG = "https://telegra.ph/file/8fc05e1bc37effdf4302b.jpg"
+    ALIVE_IMG = "https://telegra.ph/file/8fc05e1bc37effdf4302b.jpg"
+    UPDATE_CHANNEL = "WizkhalifaBots" # Your own channel for updates, do not add the @
+    SUPPORT_CHAT = "smartkillersda"  # Your own group for support, do not add the @
     JOIN_LOGGER = (-10012345678)  # A new channel ID To log who started the bot. Starting with "-100", Put inside braces
     EVENT_LOGS = (-10012345678)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    MONGO_DB_URI = "" 
-    SQLALCHEMY_DATABASE_URI = ""  # needed for any database module
+    MONGO_DB_URI = "ongodb+srv://King098:king098@cluster0.lhmvji8.mongodb.net/?retryWrites=true&w=majority" 
+    SQLALCHEMY_DATABASE_URI = "postgres://mcclbjwx:CqMrbec47cqL5KbaZOUDlVQWOscjNcKR@peanut.db.elephantsql.com/mcclbjwx"  # needed for any database module
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "pu8g0S09t9vrpGW1jXvuHzxQq2NdrJCE2w47Zhq8AxYjI47R1hmU95dUh61ZWbw1"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
     TEMP_DOWNLOAD_DIRECTORY = "./"
     
